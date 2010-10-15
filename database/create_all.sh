992d212518a6d7fb18dab99790ffc8ba87e99e7e
Jason Brownlee <jason.brownlee05@gmail.com>
2010-10-15 13:19:40 +1100

    added script to create all db tables

---
+++ database/create_all.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+mysql -u root < create_tables.sql
+
+mysql -u root < sales_vd_inserts.sql
+mysql -u root < store_vd_inserts.sql
+mysql -u root < template_vd_inserts.sql
+
+mysql -u root < issue_mo_inserts.sql
+mysql -u root < sales_mo_inserts.sql
+mysql -u root < store_mo_inserts.sql
+mysql -u root < wholesaler_mo_inserts.sql
+
+mysql -u root < zip_plus4_data1_inserts.sql
+mysql -u root < zip_plus4_data2_inserts.sql
+mysql -u root < zip_plus4_data3_inserts.sql
+mysql -u root < zip_plus4_data4_inserts.sql
+mysql -u root < zip_plus4_data5_inserts.sql
