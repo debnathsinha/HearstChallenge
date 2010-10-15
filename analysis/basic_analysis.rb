4044c49554b2e80677116493178cde6cbb395bc0
Jason Brownlee <jason.brownlee05@gmail.com>
2010-10-15 14:58:44 +1100

    calculate basic summary statistics

--- analysis/basic_analysis.rb
+++ analysis/basic_analysis.rb
@@ -9,7 +9,7 @@ def print_size(name, size)
   printf("| %s | %i |\n", name, size)
 end
 
-def print_number(size)
+def print_number(name, size)
   printf("| %s | %.3f |\n", name, size)
 end
 
@@ -36,8 +36,15 @@ end
 def print_template_vd_stats  
   puts "validation data info"
   print_heading(["Measures", "Values"])
-  print_size("Total records", TemplateVd.count)
   
+  print_size("Total Records", TemplateVd.count)
+  print_size("Total Stores", TemplateVd.calculate(:count, :store_key, :distinct=>true))
+  print_size("Total Titles", TemplateVd.calculate(:count, :title_key, :distinct=>true))
+  print_size("Total Months", TemplateVd.calculate(:count, :yearmonth, :distinct=>true))
+  
+  print_size("Average Records/Month", TemplateVd.count_by_sql("select avg(c) as average from (select count(*) as c from template_vd group by yearmonth) as t"))
+  print_size("Average Stores/Month", TemplateVd.count_by_sql("select avg(c) from (select count(distinct(store_key)) as c from template_vd group by yearmonth) as t"))
+  print_size("Average Titles/Month", TemplateVd.count_by_sql("select avg(c) from (select count(distinct(title_key)) as c from template_vd group by yearmonth) as t"))
   
 end
 
