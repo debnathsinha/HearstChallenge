36fb57e5964ca15ff40d204e8631c15158cb3a04
Jason Brownlee <jason.brownlee05@gmail.com>
2010-10-17 10:17:48 +1100

    fixed bug in script

--- experiments/average_issue_year_month.rb
+++ experiments/average_issue_year_month.rb
@@ -24,10 +24,13 @@ def to_key(title, year, month)
 end
 
 if __FILE__ == $0
+
   # test query
-  # rs = TemplateMo.find_by_sql("select mo.title_key as title_key, mo.on_year as on_year, mo.on_month as on_month, avg(mo.sales_total) as sales_total from template_mo mo, (select title_key, on_year, on_month from template_vd2 group by title_key, on_year, on_month) as vd where mo.title_key=vd.title_key and mo.on_year=vd.on_year and mo.on_month=vd.on_month and mo.on_year=2008 and mo.on_month=01 and mo.title_key<5 group by title_key, on_year, on_month")
+  # rs = TemplateMo.find_by_sql("select mo.title_key as title_key, mo.on_year as on_year, mo.on_month as on_month, avg(mo.sales_total) as sales_total from template_mo mo, (select title_key, on_year, on_month from template_vd2 group by title_key, on_year, on_month) as vd where mo.title_key=vd.title_key and mo.on_year=vd.on_year and mo.on_month=vd.on_month and mo.on_year=2008 and mo.on_month=01 and mo.title_key<10 group by title_key, on_year, on_month")
+
   # actual query
   rs = TemplateMo.find_by_sql("select mo.title_key as title_key, mo.on_year as on_year, mo.on_month as on_month, avg(mo.sales_total) as sales_total from template_mo mo, (select title_key, on_year, on_month from template_vd2 group by title_key, on_year, on_month) as vd where mo.title_key=vd.title_key and mo.on_year=vd.on_year and mo.on_month=vd.on_month group by title_key, on_year, on_month")
+
   puts "Result set: #{rs.length}"
   
   # build a hash
@@ -49,22 +52,17 @@ if __FILE__ == $0
       next #skip
     end
     store, title, yearmonth = line.split(",")
-    year, month = yearmonth[0,4], yearmonth[4,5]
+    year, month = yearmonth[0,4].to_i, yearmonth[4,5].to_i
     # get average across all stores for title/year/month
     key = to_key(title, year, month)
     avg = hash[key]
-    if avg.nil? 
-      missing_count += 1
-      avg = 9.9273
-    end
-    # raise "could not find average for key=(#{key}) line #{(i+1)} = (#{line})" if avg.nil?    
-    # avg = TemplateMo.average(:sales_total, :conditions=>['title_key=? and on_year=? and on_month=?',title,year,month])    
+    raise "could not find average for key=(#{key}) line #{(i+1)} = (#{line})" if avg.nil?
     output_lines << "#{store},#{title},#{yearmonth},#{avg}"
     puts "#{i}..." if (i%1000)==0
     total += 1
   end
   
-  puts "No value for #{missing_count} of #{total} rows"
+  # puts "No. value for #{missing_count} of #{total} rows"
   
   # write file
   f = File.open("average_issue_year_month_submission.csv", 'w')
