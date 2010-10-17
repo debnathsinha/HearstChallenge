fe51a0d794a7ab29c842db8d4a769b0a7144307a
Jason Brownlee <jason.brownlee05@gmail.com>
2010-10-17 16:49:47 +1100

    updated tables

--- database/create_indexes.sql
+++ database/create_indexes.sql
@@ -13,3 +13,6 @@ CREATE INDEX title_key_index USING BTREE ON sales_mo (title_key);
 CREATE INDEX issue_key_index USING BTREE ON sales_mo (issue_key);
 CREATE INDEX store_key_index USING BTREE ON sales_mo (store_key);
 CREATE INDEX wholesaler_key_index USING BTREE ON sales_mo (wholesaler_key);
+
+-- template_vd
+CREATE INDEX template_td_key_index_1 USING BTREE ON template_vd (store_key, title_key, yearmonth);
--- database/create_secondary_tables.sql
+++ database/create_secondary_tables.sql
@@ -3,6 +3,7 @@
 connect hearst_challenge;
 
 -- issue_mo_2
+drop table IF EXISTS issue_mo_2;
 create table issue_mo_2 (
 	issue_key int not null primary key,
 	on_year int not null,
@@ -17,7 +18,9 @@ from issue_mo i;
 
 
 -- template_mo
+drop table IF EXISTS template_mo;
 create table template_mo (
+	wholesaler_key int not null, 
 	store_key int not null, 
 	title_key int not null, 
 	on_year int not null,
@@ -25,17 +28,20 @@ create table template_mo (
 	sales_total int  not null
 );
 
-CREATE INDEX template_mo_key_index USING BTREE ON template_mo (store_key, title_key, on_year, on_month);
+CREATE INDEX template_mo_key_index_1 USING BTREE ON template_mo (store_key, title_key, on_year, on_month);
+CREATE INDEX template_mo_key_index_2 USING BTREE ON template_mo (wholesaler_key, store_key, title_key, on_year, on_month);
 
-insert into template_mo (store_key, title_key, on_year, on_month, sales_total)
-select smo.store_key, smo.title_key, imo.on_year, imo.on_month, sum(smo.sales)
+insert into template_mo (wholesaler_key, store_key, title_key, on_year, on_month, sales_total)
+select smo.wholesaler_key, smo.store_key, smo.title_key, imo.on_year, imo.on_month, sum(smo.sales)
 from issue_mo_2 imo, sales_mo smo
 where smo.issue_key = imo.issue_key
 group by smo.store_key, smo.title_key, imo.on_year, imo.on_month;
 
 
 -- template_td2
+drop table IF EXISTS template_vd2;
 create table template_vd2 (
+	wholesaler_key int not null, 
 	store_key int not null, 
 	title_key int not null, 
 	on_year int not null,
@@ -43,8 +49,11 @@ create table template_vd2 (
 	sales_total int
 );
 
-CREATE INDEX template_td2_key_index USING BTREE ON template_vd2 (store_key, title_key, on_year, on_month);
+CREATE INDEX template_td2_key_index_1 USING BTREE ON template_vd2 (store_key, title_key, on_year, on_month);
+CREATE INDEX template_td2_key_index_2 USING BTREE ON template_vd2 (wholesaler_key, store_key, title_key, on_year, on_month);
 
-insert into template_vd2 (store_key, title_key, on_year, on_month, sales_total)
-select store_key, title_key, CONVERT(substring(yearmonth, 1, 4), SIGNED), CONVERT(substring(yearmonth, 5, 2), SIGNED), sales_total
-from template_vd;
+insert into template_vd2 (wholesaler_key, store_key, title_key, on_year, on_month, sales_total)
+select s.wholesaler_key, v.store_key, v.title_key, 
+CONVERT(substring(v.yearmonth, 1, 4), SIGNED), CONVERT(substring(v.yearmonth, 5, 2), SIGNED), v.sales_total
+from template_vd v, sales_vd s
+where v.store_key = s.store_key
