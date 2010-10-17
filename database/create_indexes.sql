b4c9c6c99506f2c9de76fd03f3feb1db6064df9f
Jason Brownlee <jason.brownlee05@gmail.com>
2010-10-17 11:21:41 +1100

    working from a wholesaler angle

--- database/create_indexes.sql
+++ database/create_indexes.sql
@@ -3,7 +3,7 @@ connect hearst_challenge;
 -- sales_vd
 CREATE INDEX title_key_index USING BTREE ON sales_vd (title_key);
 CREATE INDEX issue_key_index USING BTREE ON sales_vd (issue_key);
-
+CREATE INDEX wholesaler_key_index USING BTREE ON sales_vd (wholesaler_key);
 
 -- issue_mo
 CREATE INDEX issue_key_index USING BTREE ON issue_mo (issue_key);
@@ -12,3 +12,4 @@ CREATE INDEX issue_key_index USING BTREE ON issue_mo (issue_key);
 CREATE INDEX title_key_index USING BTREE ON sales_mo (title_key);
 CREATE INDEX issue_key_index USING BTREE ON sales_mo (issue_key);
 CREATE INDEX store_key_index USING BTREE ON sales_mo (store_key);
+CREATE INDEX wholesaler_key_index USING BTREE ON sales_mo (wholesaler_key);
