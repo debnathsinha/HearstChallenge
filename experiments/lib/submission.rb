c9e8d445397c063347b871693631899c90293bf6
Jason Brownlee <jason.brownlee05@gmail.com>
2010-10-24 08:02:09 +1100

    faster file loading

--- experiments/lib/submission.rb
+++ experiments/lib/submission.rb
@@ -13,25 +13,31 @@ class Submission
   def get_sales(store, title, year, month)
     raise "get_sales(store, title, year, month) is undefined"
   end
-  
+
   def generate_submission
-    output_lines = []
-    # build output
-    puts "Processing template file: '#{@template_filename}'"
-    File.open(@template_filename).each_with_index do |line, i|
+    # load template
+    lines = File.open(@template_filename).readlines
+    puts "Processing #{lines.size} lines of template file: '#{@template_filename}'"
+    # process lines
+    output_lines = []    
+    lines.each_with_index do |line, i|
       line = line.strip
-      if i==0
-        output_lines << line
-        next #skip
+      if i==0 
+        output_lines << line; next; 
       end
-      store, title, yearmonth = line.split(",")
-      year, month = yearmonth[0,4], yearmonth[4,5]
-      sales = get_sales(store.to_i, title.to_i, year.to_i, month.to_i)
-      output_lines << "#{store},#{title},#{yearmonth},#{sales}"
-      puts "processed #{i} lines..." if (i%20000)==0
+      store, title, year, month = parse_line(line)      
+      sales = get_sales(store, title, year, month)
+      output_lines << "#{store},#{title},#{year}#{month},#{sales}"
+      puts "processed #{i} lines..." if (i%20000)==0      
     end
-    # write file
-    write_output_file(output_lines, output_filename)
+    # write output
+   write_output_file(output_lines, output_filename)
+  end
+  
+  def parse_line(line)
+    store, title, yearmonth = line.split(",")
+    year, month = yearmonth[0,4], yearmonth[4,5]
+    return [store.to_i, title.to_i, year.to_i, month.to_i]
   end
   
   def write_output_file(lines_array, filename)
